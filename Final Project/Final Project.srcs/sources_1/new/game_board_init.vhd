94cf3296f29fcd12a953b7105eec70486a223e98
Stoll <bstoll@stevens.edu>
2023-03-28 13:35:45 -0400

    writing code for game_board_init

--- Final Project/Final Project.srcs/sources_1/new/game_board_init.vhd
+++ Final Project/Final Project.srcs/sources_1/new/game_board_init.vhd
@@ -1,42 +1,31 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/28/2023 12:48:57 PM
--- Design Name: 
--- Module Name: game_board_init - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+--set where the piece is and therefore where the piece is not (background)
+--start with the square and then use shifts to make two separate pieces that act as 1 piece
 entity game_board_init is
---  Port ( );
+    Port (
+            v_sync      : IN std_logic;
+            pixel_row   : IN std_logic;
+            pixel_col   : IN std_logic;
+            red         : IN std_logic;
+            green       : IN std_logic;
+            blue        : IN std_logic
+    );
 end game_board_init;
 
 architecture Behavioral of game_board_init is
-
+    constant block_w : integer := 5; -- block width should be 5 pixels
+    constant block_h : integer := 5; -- block hieght should be 5 px
+    --speed of the block (5px/var rate) for now keep constant
+    constant block_speed : std_logic_vector (10 downto 0) := conv_std_logic_vector (6, 11);
+    signal block_on : std_logic;
+    signal game_on : std_logic := '0';
+    --current block position
+    signal block_x : std_logic_vector(10 downto 0) := conv_std_logic_vector (400, 11);
+    signal block_y : std_logic_vector(10 downto 0) := conv_std_logic_vector (300, 11);
 begin
 
 
