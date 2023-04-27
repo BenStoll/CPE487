6e8506abde9c98d3f6827823be3f96b4518a42e0
Nicholas Piotrowski <71047019+nicholaspiotrowski@users.noreply.github.com>
2023-04-27 12:59:56 -0400

    Update tetris.vhd

--- FinalProject/Code/Orig/tetris.vhd
+++ FinalProject/Code/Orig/tetris.vhd
@@ -7,33 +7,44 @@ USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 ENTITY tetris IS 
         PORT (
         clk_in    : IN STD_LOGIC;
-        vga_red   : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-        vga_green : OUT STD_LOGIC_VECTOR (2 DOWNTO 0);
-        vga_blue  : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
-        vga_hsync : OUT STD_LOGIC;
-        vga_vsync : OUT STD_LOGIC
+        VGA_red   : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        VGA_green : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        VGA_blue  : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        VGA_hsync : OUT STD_LOGIC;
+        VGA_vsync : OUT STD_LOGIC;
+        btn0 : IN STD_LOGIC
     );
 END tetris;
-
 
 ARCHITECTURE Behavioral OF tetris IS
     SIGNAL pxl_clk : STD_LOGIC;
     -- internal signals to connect modules
-    SIGNAL S_red, S_green, S_blue : STD_LOGIC;
+    SIGNAL S_red, S_green, S_blue : STD_LOGIC; --_VECTOR (3 DOWNTO 0);
     SIGNAL S_vsync : STD_LOGIC;
-    SIGNAL S_pixel_row, S_pixel_col : STD_LOGIC_VECTOR (10 DOWNTO 0);
+    SIGNAL S_pixel_row, S_pixel_col : STD_LOGIC_VECTOR (10 DOWNTO 0);    
     
-    
+    component piece_behav is 
+    port(
+        v_sync : IN STD_LOGIC;
+        pixel_row : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+        pixel_col : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+        --bat_x : IN STD_LOGIC_VECTOR (10 DOWNTO 0); -- current bat x position
+        serve : IN STD_LOGIC; -- initiates serve
+        red : OUT STD_LOGIC;
+        green : OUT STD_LOGIC;
+        blue : OUT STD_LOGIC
+    );
+    end component;
     
     COMPONENT vga_sync IS
         PORT (
             pixel_clk : IN STD_LOGIC;
-            red_in    : IN STD_LOGIC;
-            green_in  : IN STD_LOGIC;
-            blue_in   : IN STD_LOGIC;
-            red_out   : OUT STD_LOGIC;
-            green_out : OUT STD_LOGIC;
-            blue_out  : OUT STD_LOGIC;
+            red_in    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            green_in  : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            blue_in   : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            red_out   : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+            green_out : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+            blue_out  : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
             hsync     : OUT STD_LOGIC;
             vsync     : OUT STD_LOGIC;
             pixel_row : OUT STD_LOGIC_VECTOR (10 DOWNTO 0);
@@ -41,6 +52,7 @@ ARCHITECTURE Behavioral OF tetris IS
         );
     END COMPONENT;
     
+        
     component clk_wiz_0 is
     port (
       clk_in1  : in std_logic;
@@ -48,25 +60,33 @@ ARCHITECTURE Behavioral OF tetris IS
     );
     end component;
     
-    
-    
     BEGIN
     
     --Add the piece behavior here
     
+    piece_behavior : piece_behav
     
+    PORT MAP(
+        v_sync      => S_vsync,
+        pixel_row   => S_pixel_row,
+        pixel_col   => S_pixel_col,
+        serve       => btn0,
+        red         => S_red, 
+        green       => S_green, 
+        blue        => S_blue
+    );
     
     
     vga_driver : vga_sync
     PORT MAP(
         --instantiate vga_sync component
         pixel_clk => pxl_clk, 
-        red_in    => S_red, 
-        green_in  => S_green, 
-        blue_in   => S_blue, 
-        red_out   => vga_red(2), 
-        green_out => vga_green(2), 
-        blue_out  => vga_blue(1), 
+        red_in    => S_red & "000", 
+        green_in  => S_green & "000", 
+        blue_in   => S_blue & "000", 
+        red_out   => vga_red, 
+        green_out => vga_green, 
+        blue_out  => vga_blue, 
         pixel_row => S_pixel_row, 
         pixel_col => S_pixel_col, 
         hsync     => vga_hsync, 
