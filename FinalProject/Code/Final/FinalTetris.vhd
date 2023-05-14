6ce58966b55bb2e94a1a23d67c16db36f0945445
Ben Stoll <98097869+BenStoll@users.noreply.github.com>
2023-05-14 15:42:50 -0400

    Create FinalTetris.vhd

---
+++ FinalProject/Code/Final/FinalTetris.vhd
@@ -0,0 +1,106 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+-- This is pulling code from vga_top as that is where the instantiation of the code is in Lab 3
+--In piece behavior, the code is coming from ball.vhd
+
+ENTITY tetris IS 
+        PORT (
+        clk_in    : IN STD_LOGIC;
+        vga_red   : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        vga_green : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        vga_blue  : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+        vga_hsync : OUT STD_LOGIC;
+        vga_vsync : OUT STD_LOGIC;
+        piece_package : IN STD_LOGIC;
+        btn0 : IN STD_LOGIC
+    );
+END tetris;
+
+
+ARCHITECTURE Behavioral OF tetris IS
+    SIGNAL pxl_clk : STD_LOGIC;
+    -- internal signals to connect modules
+    SIGNAL S_red, S_green, S_blue : STD_LOGIC;
+    SIGNAL S_vsync : STD_LOGIC;
+    SIGNAL S_pixel_row, S_pixel_col : STD_LOGIC_VECTOR (10 DOWNTO 0);
+
+
+    COMPONENT vga_sync IS
+        PORT (
+            pixel_clk : IN STD_LOGIC;
+            red_in    : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            green_in  : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            blue_in   : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+            red_out   : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+            green_out : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+            blue_out  : OUT STD_LOGIC_VECTOR (3 DOWNTO 0);
+            hsync     : OUT STD_LOGIC;
+            vsync     : OUT STD_LOGIC;
+            pixel_row : OUT STD_LOGIC_VECTOR (10 DOWNTO 0);
+            pixel_col : OUT STD_LOGIC_VECTOR (10 DOWNTO 0)
+        );
+    END COMPONENT;
+    
+    
+    component clk_wiz_0 is
+    port (
+      clk_in1  : in std_logic;
+      clk_out1 : out std_logic
+    );
+    end component;
+    
+    
+    component piece_behav is 
+    port(
+        v_sync : IN STD_LOGIC;
+        pixel_row : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+        pixel_col : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+        serve : IN STD_LOGIC; -- initiates serve
+        red : OUT STD_LOGIC;
+        green : OUT STD_LOGIC;
+        blue : OUT STD_LOGIC
+    );
+    end component;
+    
+        
+    BEGIN
+    
+    piece_behavior : piece_behav
+    PORT MAP(
+        v_sync => S_vsync,
+        pixel_row => S_pixel_row,
+        pixel_col => S_pixel_col,
+        serve => btn0,
+        red => S_red, 
+        green => S_green, 
+        blue => S_blue
+    );
+    
+    
+    vga_driver : vga_sync
+    PORT MAP(
+        --instantiate vga_sync component
+        pixel_clk => pxl_clk, 
+        red_in    => S_red & "0000", 
+        green_in  => S_green & "0000", 
+        blue_in   => S_blue & "0000",
+        red_out   => vga_red, 
+        green_out => vga_green, 
+        blue_out  => vga_blue, 
+        pixel_row => S_pixel_row, 
+        pixel_col => S_pixel_col, 
+        hsync     => vga_hsync, 
+        vsync     => S_vsync
+    );
+    vga_vsync <= S_vsync; --connect output vsync
+        
+    clk_wiz_0_inst : clk_wiz_0
+    port map (
+      clk_in1 => clk_in,
+      clk_out1 => pxl_clk
+    );
+    
+    
+END Behavioral;
